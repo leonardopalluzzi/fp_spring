c90322f968746d0f00372aa913fec0629180a0b6
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-08-04 01:49:57 +0200

    edit filter chain for jwt implementation

---
+++ src/main/java/org/finalproject/java/fp_spring/Controllers/AdminController.java
@@ -0,0 +1,10 @@
+package org.finalproject.java.fp_spring.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+}
--- src/main/java/org/finalproject/java/fp_spring/Security/SecurityConfiguration.java
+++ src/main/java/org/finalproject/java/fp_spring/Security/SecurityConfiguration.java
@@ -6,12 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
+
+    // QUI BISOGNA SEPARARE LA SICUREZZA DELL'MVC DA QUELLA DELL'API CON 2 FILTER
+    // CHAIN SEPARATE
 
     // filter chain
     @Bean
@@ -19,10 +24,15 @@ public class SecurityConfiguration {
         http
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/admin/**").hasAuthority("ADMIN")
-                        .requestMatchers("/operator/**").hasAuthority("OPERATOR")
-                        .requestMatchers("/company/**").hasAnyAuthority("COMPANY_ADMIN", "COMPANY_USER")
-                        .anyRequest().permitAll())
-                .formLogin(Customizer.withDefaults());
+                        .requestMatchers("/api/v1/company/**").authenticated()
+                        .requestMatchers("/api/v1/client/**").authenticated()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().denyAll())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // per API REST
+                .formLogin(form -> form
+                        .loginPage("/admin/login") // solo per admin MVC
+                        .permitAll())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
