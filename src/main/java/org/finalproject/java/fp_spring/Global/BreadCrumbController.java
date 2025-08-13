ec604d98b82d40bb50c01dd6d0852e3ade92327d
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-08-13 22:55:59 +0200

    fix breadcrumb 2

--- src/main/java/org/finalproject/java/fp_spring/Global/BreadCrumbController.java
+++ src/main/java/org/finalproject/java/fp_spring/Global/BreadCrumbController.java
@@ -18,50 +18,55 @@ public class BreadCrumbController {
         String uri = request.getRequestURI();
         String returnTo = request.getParameter("returnTo");
         String isService = request.getParameter("isService");
+        String isOperator = request.getParameter("isOperator");
+
         boolean serviceFlag = Boolean.parseBoolean(isService);
+        boolean operatorFlag = Boolean.parseBoolean(isOperator);
 
         List<Map<String, String>> breadcrumb = new ArrayList<>();
 
         breadcrumb.add(Map.of("label", "Home", "url", "/"));
 
+        // Company
         if (uri.startsWith("/admin/company")) {
+            breadcrumb.add(Map.of("label", "Dashboard", "url", "/admin/company"));
+
             if (uri.matches("/admin/company/edit/\\d+")) {
-                breadcrumb.add(Map.of("label", "Dashboard", "url", "/admin/company"));
+
                 breadcrumb.add(Map.of("label", "Edit Company Details", "url", uri));
             } else if (uri.matches("/admin/company/\\d+")) {
                 // show
-                breadcrumb.add(Map.of("label", "Dashboard", "url", "/admin/company"));
                 breadcrumb.add(Map.of("label", "View Company Details", "url", uri));
             } else if (uri.contains("/create")) {
-                breadcrumb.add(Map.of("label", "Dashboard", "url", "/admin/company"));
                 breadcrumb.add(Map.of("label", "Create Company", "url", uri));
-            } else {
-                breadcrumb.add(Map.of("label", "Dashboard", "url", "/admin/company"));
             }
 
+            // Users
         } else if (uri.startsWith("/admin/users")) {
             breadcrumb.add(Map.of("label", "Dashboard", "url", "/admin/company"));
-            if (uri.matches("/admin/users/\\d+")) {
-                // index
-                if (serviceFlag) {
 
+            // index / show
+            if (uri.matches("/admin/users/\\d+")) {
+                if (serviceFlag) {
+                    String label = operatorFlag ? "Employees" : "Customers";
                     breadcrumb.add(Map.of("label", "Company Details", "url", returnTo));
-                    breadcrumb.add(Map.of("label", "Users", "url", uri));
-                } else if (!serviceFlag) {
+                    breadcrumb.add(Map.of("label", label, "url", returnTo));
+                } else {
                     breadcrumb.add(Map.of("label", "Users", "url", uri));
                 }
 
+                // edit
             } else if (uri.matches("/admin/users/edit/\\d+")) {
                 if (serviceFlag) {
+                    String label = operatorFlag ? "Employees" : "Customers";
                     breadcrumb.add(Map.of("label", "Company Details", "url", returnTo));
-                    breadcrumb.add(Map.of("label", "Users", "url", returnTo));
-                    breadcrumb.add(Map.of("label", "Edit User", "url", uri));
-
-                } else if (!serviceFlag) {
-                    breadcrumb.add(Map.of("label", "Users", "url", returnTo));
-                    breadcrumb.add(Map.of("label", "Edit User", "url", uri));
+                    breadcrumb.add(Map.of("label", label, "url", returnTo));
+                } else {
+                    breadcrumb.add(Map.of("label", "Customers", "url", returnTo));
                 }
+                breadcrumb.add(Map.of("label", "Edit User", "url", uri));
 
+                // Create
             } else if (uri.contains("/create")) {
                 breadcrumb.add(Map.of("label", "Users", "url", returnTo));
                 breadcrumb.add(Map.of("label", "Create User", "url", uri));
