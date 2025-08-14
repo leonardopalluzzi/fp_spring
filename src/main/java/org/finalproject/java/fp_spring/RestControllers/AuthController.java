7cee4154ed241042c0822c55e832f0f3701958cb
leonardopalluzzi <lp.palluzzi@gmail.com>
2025-08-14 17:24:48 +0200

    remove useless import

--- src/main/java/org/finalproject/java/fp_spring/RestControllers/AuthController.java
+++ src/main/java/org/finalproject/java/fp_spring/RestControllers/AuthController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,14 +29,14 @@ public class AuthController {
 
     // register
     @PostMapping("/regiser")
-    public ResponseEntity register(@RequestBody User user) {
+    public ResponseEntity<String> register(@RequestBody User user) {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     // logout
     @PostMapping("/logout")
-    public ResponseEntity logout(@RequestBody String token) {
+    public ResponseEntity<String> logout(@RequestBody String token) {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
