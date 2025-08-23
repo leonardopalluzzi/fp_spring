1161b4086cbb38ad7d14b55dff7ad92549d7eae4
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-08-23 19:51:38 +0200

    start working on create employee method

---
+++ src/main/java/org/finalproject/java/fp_spring/DTOs/UserInputDTO.java
@@ -0,0 +1,34 @@
+package org.finalproject.java.fp_spring.DTOs;
+
+//dto usato per la creazione di nuovi impiegati
+public class UserInputDTO {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
--- src/main/java/org/finalproject/java/fp_spring/RestControllers/UsersRestController.java
+++ src/main/java/org/finalproject/java/fp_spring/RestControllers/UsersRestController.java
@@ -4,6 +4,7 @@ import java.nio.file.AccessDeniedException;
 
 import org.finalproject.java.fp_spring.DTOs.UserAdminIndexDTO;
 import org.finalproject.java.fp_spring.DTOs.UserDTO;
+import org.finalproject.java.fp_spring.DTOs.UserInputDTO;
 import org.finalproject.java.fp_spring.Enum.RoleName;
 import org.finalproject.java.fp_spring.Exceptions.NotFoundException;
 import org.finalproject.java.fp_spring.Security.config.DatabaseUserDetails;
@@ -12,13 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -75,6 +82,19 @@ public class UsersRestController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(e.getMessage());
         }
+
+    }
+
+    @PutMapping("/store/{id}")
+    @PreAuthorize("hasAuthority(RoleName.COMPANY_ADMIN.ToString())")
+    public ResponseEntity<?> store(@Valid @RequestBody UserInputDTO user, BindingResult bindingResult,
+            @PathVariable("id") Integer serviceId) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body("There are errors in some fields");
+        }
+
+        DatabaseUserDetails currentUser = (DatabaseUserDetails) SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal();
 
     }
 
