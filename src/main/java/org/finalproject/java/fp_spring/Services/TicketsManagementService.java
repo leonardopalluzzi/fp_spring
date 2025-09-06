31a9207b97eb890cc88368d43110042f3c275a1f
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-09-06 05:46:17 +0200

    fix get ticket history

--- src/main/java/org/finalproject/java/fp_spring/Services/TicketsManagementService.java
+++ src/main/java/org/finalproject/java/fp_spring/Services/TicketsManagementService.java
@@ -23,6 +23,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -103,7 +104,7 @@ public class TicketsManagementService {
             throw new AccessDeniedException("You don't have the authority to access this resource");
         }
 
-        Pageable pagination = PageRequest.of(page, 10);
+        Pageable pagination = PageRequest.of(page, 10, Sort.by("changedAt").descending());
         Page<TicketHistory> ticketHistoryEntityPaged = ticketHistoryRepo.findAllByTicketId(ticketId, pagination);
 
         Page<TicketHistoryDTO> ticketHistoryDTOPaged = ticketHistoryEntityPaged.map(mapper::toTicketHistoryDTO);
